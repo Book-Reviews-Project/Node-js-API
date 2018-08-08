48a94ab3ce842a9ea1188d1e0640e9ba40014d5e
Emiliyan Panagyurski <emiliyan.panagyurski@techhuddle.com>
2018-08-08 15:26:29 +0300

    created user end-points

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const userRoutes = require('./server/routes').UserRouter;
 
 // Set up the express app
 const app = express();
@@ -11,6 +12,30 @@ app.use(logger('dev'));
 // Parse incoming requests data (https://github.com/expressjs/body-parser)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+	res.header('Access-Controll-Allow-Origin', '*');
+	res.header(
+		'Access-Controll-Allow-Headers',
+		'Origin, X-Request-With, Content-Type, Accept, Authorization');
+
+	if(req.method === 'OPTIONS') {
+		res.header(
+			'Access-Controll-Allow-Methods',
+			'PUT, POST, PATCH, DELETE, GET');
+	}
+
+	next();
+});
+
+// use routes
+app.use('/api/v1/user', userRoutes);
+
+app.use((req, res) => {
+	res.status(404).json({
+		message: 'Not Found'
+	});
+});
 
 // Setup a default catch-all route that sends back a welcome message in JSON format.
 app.get('*', (req, res) => res.status(200).send({
---
+++ server/controllers/user-controller.js
@@ -0,0 +1,106 @@
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+const User = require('../models').user;
+const createToken = require('../utils/tokens').createToken;
+
+module.exports = {
+	async create(req, res) {
+		try {
+			const hash = await bcrypt.hash(req.body.password, saltRounds);
+			const user = await User.create({name: req.body.name, password: hash, email: req.body.email});
+			const token = await createToken({
+				name: user.name,
+				email: user.email
+			},
+			process.env.TOKEN_KEY,
+			{
+				expiresIn: '1h'
+			});
+			return res.status(200).json({
+				message: 'user created',
+				token: token,
+				user: {
+					name: user.name,
+					image: user.image,
+					email: user.email,
+					createdAt: user.createdAt,
+					updatedAt: user.updatedAt
+				}
+			});
+
+		} catch(err) {
+			return res.status(400).json({
+				message: err.message
+			});
+		}
+	},
+	async signin(req, res) {
+		try {
+			const user = await User.findOne({where: {email: req.body.email}});
+      
+			if(!user){
+				return res.status(401).json({
+					message: 'Auth failed'
+				});
+			}
+
+			const match = await bcrypt.compare(req.body.password, user.password);
+
+			if(match) {
+				const token = await createToken({
+					name: user.name,
+					email: user.email
+				},
+				process.env.TOKEN_KEY,
+				{
+					expiresIn: '1h'
+				});
+				return res.status(202).json({
+					message: 'Auth successful',
+					token: token,
+					user: {
+						name: user.name,
+						image: user.image,
+						email: user.email,
+						createdAt: user.createdAt,
+						updatedAt: user.updatedAt
+					}
+				});
+			} else {
+				return res.status(401).json({
+					message: 'Auth failed'
+				});
+			}
+		} catch (err) {
+			return res.status(400).json({
+				message: err.message
+			});
+		}
+	},
+	async get(req, res) {
+		try{
+			const user = await User.findOne({where: {name: req.params.name}});
+
+			if(!user) {
+				return res.status(404).json({
+					messgae: 'No such user'
+				});
+			}
+
+			return res.status(200).json({
+				user: {
+					name: user.name,
+					image: user.image,
+					email: user.email,
+					createdAt: user.createdAt,
+					updatedAt: user.updatedAt
+				}
+			});
+		} catch (err) {
+			return res.status(400).json({
+				message: err.message
+			});
+		}
+	},
+};
