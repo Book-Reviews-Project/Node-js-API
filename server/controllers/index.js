71b190289b15cd6f4434eae12aef159d3ed8e513
Emiliyan Panagyurski <emiliyan.panagyurski@techhuddle.com>
2018-08-09 15:53:22 +0300

    refactored controller

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,4 +1,7 @@
-const userController = require('./user-controller');
+const encryptUtils = require('../utils/encrypt');
+const tokenUtils = require('../utils/tokens');
+const User = require('../models').user;
+const userController = require('./user-controller')(encryptUtils, tokenUtils, User);
 const reviewController = require('./review-controller');
 
 module.exports = {
