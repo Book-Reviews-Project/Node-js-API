330d8444122915fc7f30cc394aaaa563f1390e4a
Emiliyan Panagyurski <emiliyan.panagyurski@techhuddle.com>
2018-08-09 15:57:52 +0300

    refactored controller

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,8 +1,10 @@
 const encryptUtils = require('../utils/encrypt');
 const tokenUtils = require('../utils/tokens');
 const User = require('../models').user;
+const Review = require('../models').review;
+const Op = require('sequelize').Sequelize.Op;
 const userController = require('./user-controller')(encryptUtils, tokenUtils, User);
-const reviewController = require('./review-controller');
+const reviewController = require('./review-controller')(User, Review, Op);
 
 module.exports = {
 	userController,
--- server/controllers/review-controller.js
+++ server/controllers/review-controller.js
@@ -1,49 +1,47 @@
-const Op = require('sequelize').Sequelize.Op;
-const Review = require('../models').review;
-const User = require('../models').user;
-
-module.exports = {
-	async create(req, res) {
-		try{
-			const user = await User.findOne({where: {name: req.body.user}});
-			const review = await Review.create({
-				title: req.body.title,
-				content: req.body.content,
-				category: req.body.category,
-				user_id: user.id});
-			
-			return res.status(201).json({
-				message: 'Review created',
-				review: review
-			});
-		} catch(err) {
-			return res.status(400).json({
-				message: err.message
-			});
+module.exports = (User, Review, Op) => {
+	return{
+		async create(req, res) {
+			try{
+				const user = await User.findOne({where: {name: req.body.user}});
+				const review = await Review.create({
+					title: req.body.title,
+					content: req.body.content,
+					category: req.body.category,
+					user_id: user.id});
+				
+				return res.status(201).json({
+					message: 'Review created',
+					review: review
+				});
+			} catch(err) {
+				return res.status(400).json({
+					message: err.message
+				});
+			}
+		},
+		async get(req, res){
+			try{
+				const review = await Review.findOne({where: {title: req.params.title}});
+				return res.status(200).json({
+					review: review
+				});
+			} catch(err) {
+				return res.status(400).json({
+					message: err.message
+				});
+			}
+		},
+		async search(req, res) {
+			try{
+				const reviews = await Review.findAll({where: {title: {[Op.like]: '%' + req.params.title + '%'}}});
+				return res.status(200).json({
+					reviews: reviews
+				});
+			} catch(err) {
+				return res.status(400).json({
+					message: err.message
+				});
+			}
 		}
-	},
-	async get(req, res){
-		try{
-			const review = await Review.findOne({where: {title: req.params.title}});
-			return res.status(200).json({
-				review: review
-			});
-		} catch(err) {
-			return res.status(400).json({
-				message: err.message
-			});
-		}
-	},
-	async search(req, res) {
-		try{
-			const reviews = await Review.findAll({where: {title: {[Op.like]: '%' + req.params.title + '%'}}});
-			return res.status(200).json({
-				reviews: reviews
-			});
-		} catch(err) {
-			return res.status(400).json({
-				message: err.message
-			});
-		}
-	}
+	};
 };
